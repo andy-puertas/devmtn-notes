970a0c0c94c62daff0d3f4630e02141363c6244a
kale-stew <kylieastewart@gmail.com>
2017-09-27 15:39:29 -0700

    adding components for React Native use

---
+++ REACT NATIVE/gettingstarted.js
@@ -0,0 +1,17 @@
+// Welcome to React Native! 
+
+// SETUP IS KEY! IF YOU DO IT WRONG YOU CANNOT CHECKOUT YOUR APP
+
+// Balsamic Mockups 3
+
+// xCode: packages our code and the React Native library into an installable app and run it on the iOS simulator
+// HomeBrew: used to install node and watchman
+// Node/NPM: runs JavaScript outside of the browser. Node and NPM come together
+    // Node Package Manager - NPM is used to installing and managing dependencies
+// watchman: watches files on the hard drive and waits for them to change
+// RN CLI: React Native Command Line Interface: used to generate new React Native projects 
+
+// index.android.js and index.ios.js are essential root files 
+    // considered "entry points" to our app
+
+// 
