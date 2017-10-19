59f9481f27bb4530a19f9adecef3d6701846f270
kale-stew <kylieastewart@gmail.com>
2017-10-19 16:17:49 -0700

    bold

--- REACT.JS/2.2: HTTP requests.js
+++
@@ -1,5 +0,0 @@
-// services > getapi.js
-    // create a method that promises value
-
-// what do you do when that request arrives?
-    // set initial state of the variable to which the fetched JSON will assign
