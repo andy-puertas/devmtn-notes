57b8e4b116b02847241a7b0fc0fa3a3251b87e1f
kale-stew <kylieastewart@gmail.com>
2017-10-20 15:15:53 -0700

    packages rendering

--- Angular.js/travel-app/app/packages/packagesCtrl.js
+++ Angular.js/travel-app/app/packages/packagesCtrl.js
@@ -1,3 +1,13 @@
-angular.module('devmtnTravel').controller('packagesCtrl', function( $scope ) {
+angular.module('devmtnTravel').controller('packagesCtrl', function( $scope, $stateParams, mainSrvc ) {
+    mainSrvc.getPackageInfo().then( function( res ) {
+        $scope.allPackages = res.data;
 
+        // filter out packages that equal the country specified in URL
+        // put into a new $scope variable called packages
+        if ( $stateParams.country ) {
+            $scope.packages = $scope.allPackages.filter( function( package ) {
+                return package.country === $stateParams.country;
+            });
+        }
+    })
 });
