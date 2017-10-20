3e360329c08df779ad3c56cf63d9599323d5d337
kale-stew <kylieastewart@gmail.com>
2017-10-20 15:16:08 -0700

    thanks for booking!

--- Angular.js/travel-app/app/booked/bookedCtrl.js
+++ Angular.js/travel-app/app/booked/bookedCtrl.js
@@ -1,3 +1,17 @@
-angular.module('devmtnTravel').controller('bookedCtrl', function( $scope ) {
+angular.module('devmtnTravel').controller('bookedCtrl', function( $scope, $stateParams, mainSrvc ) {
+    mainSrvc.getPackageInfo().then( function( res ) {
+        $scope.allPackages = res.data;
 
+        // find the package object with the id that matches the id on $stateParams
+        // assign it to a new $scope variable called package 
+        if ( $stateParams.id ) {
+            $scope.packageIndex = $scope.allPackages.findIndex( function( package ) {
+                return package.id === parseInt( $stateParams.id );
+            });
+
+            if ( $scope.packageIndex !== -1 ) {
+                $scope.package = $scope.allPackages[ $scope.packageIndex ];
+            }
+        };
+    })
 });
