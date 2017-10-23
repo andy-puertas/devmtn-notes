32cf80e3227112abca553cbb0818fa2d78a106b3
kale-stew <kylieastewart@gmail.com>
2017-10-23 14:20:48 -0700

    linked up details, finishing cart component

---
+++ Angular.js/swag-shop/app/details/details.component.js
@@ -0,0 +1,21 @@
+angular.module('swagShop').component('detailsComponent', {
+    templateUrl: 'app/details/details.template.html',
+    controllerAs: 'detailsCtrl',
+
+    // controller function
+    controller: function( $stateParams, swagSrvc, cartSrvc ) {
+        // assign an array called swag that equals the swag array in the swag service
+        this.swag = swagSrvc.swag;
+
+        // assign an object called item that equals the swag object with the same id as the id in the URL
+        if ( $stateParams.id ) {
+            let itemIndex = this.swag.findIndex( item => item.id === $stateParams.id );
+            this.item = this.swag[ itemIndex ];
+        };
+
+        // assign a method that takes an item param and uses it to call the add method on the cart service
+        this.addToCart = function( item ) {
+            cartSrvc.add( item );
+        };
+    }
+});
